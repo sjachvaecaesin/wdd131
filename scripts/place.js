6ec0ac9ad41e4960734b35d6a4deabd7a6f343b1
sjachvaecaesin <booglyboo544@gmail.com>
2024-07-11 05:18:57 -0400

    asdfas

---
+++ scripts/place.js
@@ -0,0 +1,13 @@
+const chillElement = document.querySelector('#chill');
+
+document.querySelector("#year").textContent = new Date().getFullYear();
+
+document.querySelector("#lastModified").textContent = `Last Modified: ${document.lastModified}`;
+
+if (8 <= 10 && 10 > 4.8) {
+    let chill = 13.12 + (0.6215 * 8) - (11.37 * 10**0.16) + (0.3965 * 8 * 10**0.16);
+    chillElement.textContent = `${chill.toFixed(1)} °C`;
+}
+else {
+    chillElement.textContent = "N/A";
+}
