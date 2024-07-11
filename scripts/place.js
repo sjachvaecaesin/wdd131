02cb5dce975fb71caa106dd7c363215a426d8811
sjachvaecaesin <booglyboo544@gmail.com>
2024-07-11 06:00:47 -0400

    enough, don't care about your one line BS

--- scripts/place.js
+++ scripts/place.js
@@ -4,10 +4,14 @@ document.querySelector("#year").textContent = new Date().getFullYear();
 
 document.querySelector("#lastModified").textContent = `Last Modified: ${document.lastModified}`;
 
-if (8 <= 10 && 10 > 4.8) {
-    let chill = 13.12 + (0.6215 * 8) - (11.37 * 10**0.16) + (0.3965 * 8 * 10**0.16);
-    chillElement.textContent = `${chill.toFixed(1)} °C`;
+function calculateWindChill() {
+    if (8 <= 10 && 10 > 4.8) {
+        let chill = 13.12 + (0.6215 * 8) - (11.37 * 10**0.16) + (0.3965 * 8 * 10**0.16);
+        chillElement.textContent = `${chill.toFixed(1)} °C`;
+    }
+    else {
+        chillElement.textContent = "N/A";
+    }
 }
-else {
-    chillElement.textContent = "N/A";
-}
+
+calculateWindChill();
